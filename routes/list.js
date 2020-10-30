832a827267d4a106ff581adc7b7a91b19838e8d4
unknown <jreidmke@gmail.com>
2020-10-30 12:12:03 -0500

    completed routing

--- routes/list.js
+++ routes/list.js
@@ -43,14 +43,44 @@ router.post('/', async(req, res, next) => {
 
 router.patch('/:name', async(req, res, next) => {
     //selects item by name. updates properties
-
+    const { name, price } = req.body;
+    try {
+        const resp = await db.query(`
+        UPDATE list
+        SET name=$1, price=$2
+        WHERE name=$3
+        RETURNING name, price
+        `, [name, price, req.params.name]);
+        if(resp.rows.length === 0) {
+            throw new ExpressError("Invalid Name", 404);
+        }
+        return res.status(200).json({updated: resp.rows[0]});
+    } catch (error) {
+        return next(error);
+    }
     //returns {“updated”: {“name”: “new popsicle”, “price”: 2.45}}
 })
 
 router.delete('/:name', async(req, res, next) => {
     //deletes item by name
+    try {
+        const resp = await db.query(`
+        DELETE FROM list
+        WHERE name=$1
+        `, [req.params.name]);
+        return res.status(202).json({message: "Deleted"})
+    } catch (error) {
+        return next(error);
+    }
 
     //returns {message: “Deleted”}
 })
 
 module.exports = router; //export router so all may benefit
+
+// if(resp.rows.length === 0) {
+//     throw new ExpressError("Invalid Name", 404)
+// }
+// UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
